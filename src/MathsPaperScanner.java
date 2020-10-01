035c9c15fd0ffd08eb048293f181d9f22d64702c
EWright212 <emilywright212@gmail.com>
2020-10-01 14:41:59 +0100

    Simple refactor naming variables

--- src/MathsPaperScanner.java
+++ src/MathsPaperScanner.java
@@ -1,20 +1,18 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class MathsPaperScanner {
     public int evaluate(String expression) {
         String[] component = expression.split(" ");
         var result = Integer.parseInt(component[0]);
 
         for (int i = 2; i < component.length; i += 2) {
+            Integer currentNumber = Integer.parseInt(component[i]);
             if (component[i - 1].equals("*")) {
-                result *= Integer.parseInt(component[i]);
+                result *= currentNumber;
             } else if (component[i - 1].equals("-")) {
-                result -= Integer.parseInt(component[i]);
+                result -= currentNumber;
             } else if (component[i - 1].equals("/")) {
-                result /= Integer.parseInt(component[i]);
+                result /= currentNumber;
             } else {
-                result += Integer.parseInt(component[i]);
+                result += currentNumber;
             }
         }
 
