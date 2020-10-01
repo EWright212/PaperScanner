28d09fd5aeff17a61fc1cf973840c130b0808dc8
Sophie <sophiebrown438@gmail.com>
2020-10-01 14:25:51 +0100

    Minor refactor to conditional

--- src/MathsPaperScanner.java
+++ src/MathsPaperScanner.java
@@ -3,23 +3,20 @@ import java.util.Arrays;
 
 public class MathsPaperScanner {
     public int evaluate(String expression) {
-
         String[] component = expression.split(" ");
         var result = 0;
         var firstInt = Integer.parseInt(component[0]);
         var secondInt = Integer.parseInt(component[2]);
         String operator = component[1];
+        result = firstInt;
 
         if (operator.equals("*")) {
-            result = (firstInt * secondInt);
-        } else if (operator.equals("-")){
-            result += firstInt;
+            result *= secondInt;
+        } else if (operator.equals("-")) {
             result -= secondInt;
-        }  else if (operator.equals("/")){
-            result += firstInt;
+        } else if (operator.equals("/")) {
             result /= secondInt;
         } else {
-            result += firstInt;
             result += secondInt;
         }
 
